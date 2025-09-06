f5e97577cbe738a921a8d3b8dc0641ab812eaa6c
Anahel Bour <bour.anahel14@gmail.com>
2025-09-06 01:14:41 -0400

    l

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ← ADD THIS LINE
+  base: '/my.portfolio/', // ← ADD THIS LINE
   server: {
     port: 3000,
     open: true
